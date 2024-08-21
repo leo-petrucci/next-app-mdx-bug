8d7343093a8b617cb224d244818d300e6f5d5733
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2024-08-21 21:38:51 +0100

    Test again

--- components/MdxComponents/MdxComponents.tsx
+++ components/MdxComponents/MdxComponents.tsx
@@ -1,11 +1,9 @@
 import dynamic from "next/dynamic";
-import { Test } from "../Test";
 
 export const Components = {
-  // Test: dynamic(() =>
-  //   import("../Test").then((module) => ({
-  //     default: module.Test,
-  //   }))
-  // ),
-  Test,
+  Test: dynamic(() =>
+    import("../Test").then((module) => ({
+      default: module.Test,
+    }))
+  ),
 };
